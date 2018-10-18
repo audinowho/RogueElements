dcf575877f9d5685e2e49842fd67561225a85f07
Audino <Audino>
2018-10-17 22:18:05 -0700

    Example 6

--- RogueElements.Examples/Ex6_Items/Example6.cs
+++ RogueElements.Examples/Ex6_Items/Example6.cs
@@ -8,7 +8,151 @@ namespace RogueElements.Examples.Ex6_Items
     {
         public static void Run()
         {
-            string title = "6: WIP";
+            string title = "6: A Map with Randomly Placed Items";
+            MapGen<MapGenContext> layout = new MapGen<MapGenContext>();
+
+            //Initialize a 6x4 grid of 10x10 cells.
+            InitGridPlanStep<MapGenContext> startGen = new InitGridPlanStep<MapGenContext>();
+            startGen.CellX = 6;
+            startGen.CellY = 4;
+
+            startGen.CellWidth = 9;
+            startGen.CellHeight = 9;
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(-4, startGen));
+
+
+
+            //Create a path that is composed of a ring around the edge
+            GridPathBranch<MapGenContext> path = new GridPathBranch<MapGenContext>();
+            path.RoomRatio = new RandRange(70);
+            path.BranchRatio = new RandRange(0, 50);
+
+            SpawnList<RoomGen<MapGenContext>> genericRooms = new SpawnList<RoomGen<MapGenContext>>();
+            //cross
+            genericRooms.Add(new RoomGenSquare<MapGenContext>(new RandRange(4, 8), new RandRange(4, 8)));
+            //round
+            genericRooms.Add(new RoomGenRound<MapGenContext>(new RandRange(5, 9), new RandRange(5, 9)));
+            path.GenericRooms = genericRooms;
+
+            SpawnList<PermissiveRoomGen<MapGenContext>> genericHalls = new SpawnList<PermissiveRoomGen<MapGenContext>>();
+            genericHalls.Add(new RoomGenAngledHall<MapGenContext>(50));
+            path.GenericHalls = genericHalls;
+
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(-4, path));
+
+
+
+            //Output the rooms into a FloorPlan
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(-2, new DrawGridToFloorStep<MapGenContext>()));
+
+
+
+
+            //Draw the rooms of the FloorPlan onto the tiled map, with 1 TILE padded on each side
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(0, new DrawFloorToTileStep<MapGenContext>(1)));
+
+
+
+            //Add the stairs up and down
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(2, new FloorStairsStep<MapGenContext, StairsUp, StairsDown>(new StairsUp(), new StairsDown())));
+
+
+            //Generate water (specified by user as Terrain 2) with a frequency of 30%, using Perlin Noise in an order of 3.
+            int terrain = 2;
+            PerlinWaterStep<MapGenContext> waterPostProc = new PerlinWaterStep<MapGenContext>(40, 3, terrain);
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(3, waterPostProc));
+
+            //Remove walls where diagonals of water exist and replace with water
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(4, new DropDiagonalBlockStep<MapGenContext>(terrain)));
+            //Remove water stuck in the walls
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(4, new EraseIsolatedStep<MapGenContext>(terrain)));
+
+
+
+
+            //Apply Items
+            SpawnList<Item> itemSpawns = new SpawnList<Item>();
+            itemSpawns.Add(new Item((int)'!'), 10);
+            itemSpawns.Add(new Item((int)']'), 10);
+            itemSpawns.Add(new Item((int)'='), 10);
+            itemSpawns.Add(new Item((int)'?'), 10);
+            itemSpawns.Add(new Item((int)'$'), 10);
+            itemSpawns.Add(new Item((int)'/'), 10);
+            itemSpawns.Add(new Item((int)'*'), 50);
+            RandomSpawnStep<MapGenContext, Item> itemPlacement = new RandomSpawnStep<MapGenContext, Item>(new PickerSpawner<MapGenContext, Item>(new LoopedRand<Item>(itemSpawns, new RandRange(10, 19))));
+            layout.GenSteps.Add(new GenPriority<GenStep<MapGenContext>>(6, itemPlacement));
+
+
+
+
+            //Run the generator and print
+            MapGenContext context = layout.GenMap(MathUtils.Rand.NextUInt64());
+            Print(context.Map, title);
+        }
+
+
+        public static void Print(Map map, string title)
+        {
+            int oldLeft = Console.CursorLeft;
+            int oldTop = Console.CursorTop;
+            Console.SetCursorPosition(0, 0);
+            StringBuilder topString = new StringBuilder("");
+            string turnString = title;
+            topString.Append(String.Format("{0,-82}", turnString));
+            topString.Append('\n');
+            for (int i = 0; i < map.Width + 1; i++)
+                topString.Append("=");
+            topString.Append('\n');
+
+            for (int y = 0; y < map.Height; y++)
+            {
+                for (int x = 0; x < map.Width; x++)
+                {
+                    Loc loc = new Loc(x, y);
+                    char tileChar = ' ';
+                    Tile tile = map.Tiles[x][y];
+                    if (tile.ID <= 0)//wall
+                        tileChar = '#';
+                    else if (tile.ID == 1)//floor
+                        tileChar = '.';
+                    else if (tile.ID == 2)//water
+                        tileChar = '~';
+                    else
+                        tileChar = '?';
+
+
+                    foreach (StairsUp entrance in map.GenEntrances)
+                    {
+                        if (entrance.Loc == loc)
+                        {
+                            tileChar = '<';
+                            break;
+                        }
+                    }
+                    foreach (StairsDown entrance in map.GenExits)
+                    {
+                        if (entrance.Loc == loc)
+                        {
+                            tileChar = '>';
+                            break;
+                        }
+                    }
+
+
+                    foreach (Item item in map.Items)
+                    {
+                        if (item.Loc == loc)
+                        {
+                            tileChar = (char)item.ID;
+                            break;
+                        }
+                    }
+
+                    topString.Append(tileChar);
+                }
+                topString.Append('\n');
+            }
+            Console.Write(topString.ToString());
         }
     }
 }
---
+++ RogueElements.Examples/Ex6_Items/MapGenContext.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RogueElements;
+
+namespace RogueElements.Examples.Ex6_Items
+{
+
+    public class MapGenContext : ITiledGenContext, IRoomGridGenContext, IViewPlaceableGenContext<StairsUp>, IViewPlaceableGenContext<StairsDown>, IPlaceableGenContext<Item>
+    {
+        public Map Map { get; set; }
+
+        public int RoomTerrain { get { return Map.ROOM_TERRAIN_ID; } }
+        public int WallTerrain { get { return Map.WALL_TERRAIN_ID; } }
+
+        ITile[][] ITiledGenContext.Tiles { get { return Map.Tiles; } }
+
+        public int Width { get { return Map.Width; } }
+        public int Height { get { return Map.Height; } }
+
+        public List<StairsUp> GenEntrances {  get { return Map.GenEntrances; } }
+        public List<StairsDown> GenExits { get { return Map.GenExits; } }
+
+        public IRandom Rand { get { return Map.Rand; } }
+
+        public MapGenContext()
+        {
+            Map = new Map();
+        }
+        
+        public void InitSeed(ulong seed)
+        {
+            Map.Rand = new ReRandom(seed);
+        }
+
+        bool ITiledGenContext.TileBlocked(Loc loc)
+        {
+            return Map.Tiles[loc.X][loc.Y].ID == Map.WALL_TERRAIN_ID;
+        }
+
+        bool ITiledGenContext.TileBlocked(Loc loc, bool diagonal)
+        {
+            return Map.Tiles[loc.X][loc.Y].ID == Map.WALL_TERRAIN_ID;
+        }
+
+
+        public virtual void CreateNew(int width, int height)
+        {
+            Map.InitializeTiles(width, height);
+        }
+
+
+        public void FinishGen() { }
+
+
+        public void InitPlan(FloorPlan plan)
+        {
+            RoomPlan = plan;
+        }
+
+        public FloorPlan RoomPlan { get; private set; }
+
+
+        public void InitGrid(GridPlan plan)
+        {
+            GridPlan = plan;
+        }
+        public GridPlan GridPlan { get; private set; }
+
+
+
+
+        List<Loc> IPlaceableGenContext<Item>.GetAllFreeTiles() { return getAllFreeTiles(getOpenTiles); }
+        List<Loc> IPlaceableGenContext<StairsUp>.GetAllFreeTiles() { return getAllFreeTiles(getOpenTiles); }
+        List<Loc> IPlaceableGenContext<StairsDown>.GetAllFreeTiles() { return getAllFreeTiles(getOpenTiles); }
+
+
+        protected delegate List<Loc> getOpen(Rect rect);
+        protected virtual List<Loc> getAllFreeTiles(getOpen func)
+        {
+            return func(new Rect(0, 0, Width, Height));
+        }
+
+        List<Loc> IPlaceableGenContext<Item>.GetFreeTiles(Rect rect) { return getOpenTiles(rect); }
+        List<Loc> IPlaceableGenContext<StairsUp>.GetFreeTiles(Rect rect) { return getOpenTiles(rect); }
+        List<Loc> IPlaceableGenContext<StairsDown>.GetFreeTiles(Rect rect) { return getOpenTiles(rect); }
+
+        protected List<Loc> getOpenTiles(Rect rect)
+        {
+            Grid.LocTest checkOp = (Loc loc) =>
+            {
+                return !isTileOccupied(loc);
+            };
+
+            return Grid.FindTilesInBox(rect.Start, rect.Size, checkOp);
+        }
+
+        bool IPlaceableGenContext<Item>.CanPlaceItem(Loc loc) { return isTileOccupied(loc); }
+        bool IPlaceableGenContext<StairsUp>.CanPlaceItem(Loc loc) { return isTileOccupied(loc); }
+        bool IPlaceableGenContext<StairsDown>.CanPlaceItem(Loc loc) { return isTileOccupied(loc); }
+
+        private bool isTileOccupied(Loc loc)
+        {
+            if (Map.Tiles[loc.X][loc.Y].ID != RoomTerrain)
+                return true;
+
+            foreach (Item item in Map.Items)
+            {
+                if (item.Loc == loc)
+                    return true;
+            }
+
+            return false;
+        }
+
+
+        void IPlaceableGenContext<Item>.PlaceItem(Loc loc, Item item)
+        {
+            Item newItem = new Item(item.ID, loc);
+            Map.Items.Add(newItem);
+        }
+        void IPlaceableGenContext<StairsUp>.PlaceItem(Loc loc, StairsUp item)
+        {
+            StairsUp stairs = item.Copy();
+            stairs.Loc = loc;
+            GenEntrances.Add(stairs);
+        }
+        void IPlaceableGenContext<StairsDown>.PlaceItem(Loc loc, StairsDown item)
+        {
+            StairsDown stairs = item.Copy();
+            stairs.Loc = loc;
+            GenExits.Add(stairs);
+        }
+
+
+        int IViewPlaceableGenContext<StairsUp>.Count { get { return GenEntrances.Count; } }
+        StairsUp IViewPlaceableGenContext<StairsUp>.GetItem(int index) { return GenEntrances[index]; }
+        Loc IViewPlaceableGenContext<StairsUp>.GetLoc(int index) { return GenEntrances[index].Loc; }
+
+        int IViewPlaceableGenContext<StairsDown>.Count { get { return GenExits.Count; } }
+        StairsDown IViewPlaceableGenContext<StairsDown>.GetItem(int index) { return GenExits[index]; }
+        Loc IViewPlaceableGenContext<StairsDown>.GetLoc(int index) { return GenExits[index].Loc; }
+
+    }
+}
