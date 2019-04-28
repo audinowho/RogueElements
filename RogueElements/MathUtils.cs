f6bfd3bde761f0c232716dc6f611157f68347d50
Audino <Audino>
2019-04-27 19:33:05 -0700

    Remove unnecessary functions

--- RogueElements/MathUtils.cs
+++ RogueElements/MathUtils.cs
@@ -19,19 +19,6 @@ namespace RogueElements
             rand = new ReRandom(seed);
         }
 
-        public static int GetArc(double maxHeight, double touchdownX, double currentX)
-        {
-            // = (-4 * m / (n ^ 2) ) * x ^ 2 + (4 * m / n) * x
-            // m = height, n = total time, x = current time
-            double height = -4 * maxHeight * Math.Pow(currentX / touchdownX, 2) + 4 * maxHeight * (currentX / touchdownX);
-            return (int)Math.Round(height);
-        }
-
-        public static int Lerp(int int1, int int2, double point)
-        {
-            return (int)Math.Round(int1*(1-point) + int2*point);
-        }
-
         /// <summary>
         /// Choose a random member from a set.
         /// </summary>
