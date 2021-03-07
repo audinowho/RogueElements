ccbdc506d23097faf18552d95192adc59d2066d4
audino <2676737+audinowho@users.noreply.github.com>
2021-03-06 17:40:42 -0800

    Refactor Graph traversal

--- RogueElements/Graph.cs
+++ RogueElements/Graph.cs
@@ -12,44 +12,42 @@ namespace RogueElements
 {
     public static class Graph
     {
-        public delegate List<int> GetAdjacents(int nodeIndex);
+        public delegate List<T> GetAdjacents<T>(T nodeIndex);
 
-        public delegate void DistNodeAction(int nodeIndex, int distance);
+        public delegate void DistNodeAction<T>(T nodeIndex, int distance);
 
         /// <summary>
         /// Traverses a list of nodes. Internally handles the state of traversed/untraversed nodes.
         /// </summary>
-        /// <param name="count"></param>
+        /// <typeparam name="TID"></typeparam>
         /// <param name="start"></param>
         /// <param name="nodeAct"></param>
         /// <param name="getAdjacents"></param>
-        public static void TraverseBreadthFirst(int count, int start, DistNodeAction nodeAct, GetAdjacents getAdjacents)
+        public static void TraverseBreadthFirst<TID>(TID start, DistNodeAction<TID> nodeAct, GetAdjacents<TID> getAdjacents)
         {
             if (nodeAct == null)
                 throw new ArgumentNullException(nameof(nodeAct));
             if (getAdjacents == null)
                 throw new ArgumentNullException(nameof(getAdjacents));
 
-            int[] found = new int[count];
-            for (int ii = 0; ii < found.Length; ii++)
-                found[ii] = -1;
-            Queue<int> toExplore = new Queue<int>();
+            Dictionary<TID, int> found = new Dictionary<TID, int>();
+            Queue<TID> toExplore = new Queue<TID>();
             toExplore.Enqueue(start);
             found[start] = 0;
             while (toExplore.Count > 0)
             {
                 // take a node
-                int node = toExplore.Dequeue();
+                TID node = toExplore.Dequeue();
 
                 // act on node
                 nodeAct(node, found[node]);
 
                 // add adjacents to the END of queue
-                List<int> adjacents = getAdjacents(node);
+                List<TID> adjacents = getAdjacents(node);
                 for (int ii = 0; ii < adjacents.Count; ii++)
                 {
-                    int adjacent = adjacents[ii];
-                    if (found[adjacent] == -1)
+                    TID adjacent = adjacents[ii];
+                    if (!found.ContainsKey(adjacent))
                     {
                         toExplore.Enqueue(adjacent);
                         found[adjacent] = found[node] + 1;
