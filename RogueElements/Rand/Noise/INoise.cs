c59631e65408cd643c2babf348017bcb2c49d3b8
audino <2676737+audinowho@users.noreply.github.com>
2021-11-22 22:00:16 -0800

    Noise functions

---
+++ RogueElements/Rand/Noise/INoise.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="INoise.cs" company="Audino">
+// Copyright (c) Audino
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+namespace RogueElements
+{
+    public interface INoise
+    {
+        ulong FirstSeed { get; }
+
+        int GetInt(ulong position);
+
+        int GetInt(ulong position, int maxValue);
+
+        int GetInt(ulong position, int minValue, int maxValue);
+
+        ulong GetUInt64(ulong position);
+
+        ulong Get2DUInt64(ulong x, ulong y);
+
+        double GetDouble(ulong position);
+    }
+}
