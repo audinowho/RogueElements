b3b84abeb356247e9ab8ab395bf8ae74dd63aa6e
Audino <2676737+audinowho@users.noreply.github.com>
2022-01-15 11:51:40 -0800

    Remove extraneous function

--- RogueElements/MathUtils.cs
+++ RogueElements/MathUtils.cs
@@ -3,9 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace RogueElements
 {
@@ -13,8 +11,6 @@ namespace RogueElements
     {
         private static IRandom rand = new ReRandom();
         private static INoise noise = new ReNoise();
-
-        public delegate int CompareFunction<T>(T a, T b);
 
         public static IRandom Rand
         {
@@ -62,49 +58,6 @@ namespace RogueElements
         {
             foreach (T key in dict2.Keys)
                 AddToDictionary<T>(dict1, key, dict2[key]);
-        }
-
-        public static void AddToSortedList<T>(List<T> list, T element, CompareFunction<T> compareFunc)
-        {
-            if (compareFunc == null)
-                throw new ArgumentNullException(nameof(compareFunc));
-
-            // stable
-            int min = 0;
-            int max = list.Count - 1;
-            int point = max;
-            int compare = -1;
-
-            // binary search
-            while (min <= max)
-            {
-                point = (min + max) / 2;
-
-                compare = compareFunc(list[point], element);
-
-                if (compare > 0)
-                {
-                    // go down
-                    max = point - 1;
-                }
-                else if (compare < 0)
-                {
-                    // go up
-                    min = point + 1;
-                }
-                else
-                {
-                    // go past the last index of equal comparison
-                    point++;
-                    while (point < list.Count && compareFunc(list[point], element) == 0)
-                        point++;
-                    list.Insert(point, element);
-                    return;
-                }
-            }
-
-            // no place found
-            list.Insert(point + (compare > 0 ? 0 : 1), element);
         }
 
         public static int BiInterpolate(int topleft, int topright, int bottomleft, int bottomright, int degreeX, int xTotal, int degreeY, int yTotal)
