86e0d0bb5e3b285520b4a8a308a2ce65929f7a19
Audino <2676737+audinowho@users.noreply.github.com>
2022-01-15 11:51:40 -0800

    Remove extraneous function

--- RogueElements.Tests/MathUtilsTest.cs
+++ RogueElements.Tests/MathUtilsTest.cs
@@ -25,32 +25,5 @@ namespace RogueElements.Tests
         {
             throw new NotImplementedException();
         }
-
-        [Test]
-        public void AddToSortedListNone()
-        {
-            List<int> sortedList = new List<int>();
-
-            MathUtils.AddToSortedList(sortedList, 5, (int a, int b) => { return a - b; });
-
-            Assert.That(sortedList.Count, Is.EqualTo(1));
-            Assert.That(sortedList[0], Is.EqualTo(5));
-        }
-
-        [Test]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        [TestCase(6)]
-        public void AddToSortedListOne(int addedValue)
-        {
-            List<int> sortedList = new List<int> { 5 };
-
-            MathUtils.AddToSortedList(sortedList, addedValue, (int a, int b) => { return a - b; });
-
-            Assert.That(sortedList.Count, Is.EqualTo(2));
-            for (int ii = 1; ii < sortedList.Count; ii++)
-                Assert.That(sortedList[ii], Is.GreaterThanOrEqualTo(sortedList[ii - 1]));
-        }
     }
 }
