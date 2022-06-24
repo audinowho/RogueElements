15b4b7ef5181880e8815697071e83f93b71b23b0
Audino <2676737+audinowho@users.noreply.github.com>
2022-06-23 19:32:25 -0800

    Refactor Roomgen AskBorderFrom

--- RogueElements/MapGen/Rooms/IRoomGen.cs
+++ RogueElements/MapGen/Rooms/IRoomGen.cs
@@ -3,17 +3,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System;
+
 namespace RogueElements
 {
     public interface IRoomGen
     {
         Rect Draw { get; }
 
-        void AskWithOpenedBorder(IRoomGen sourceRoom, Dir4 dir);
-
-        void AskWithFulfillableBorder(IRoomGen sourceRoom, Dir4 dir);
-
         void AskBorderRange(IntRange range, Dir4 dir);
+
+        void AskBorderFromRoom(Rect sourceDraw, Func<Dir4, int, bool> borderQuery, Dir4 dir);
 
         bool GetOpenedBorder(Dir4 dir, int index);
 
