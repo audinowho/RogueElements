5f67c31a679337591f0e2830ca940bf87e3cf03a
AlexanderGroeger <alex.h.groeger@gmail.com>
2023-07-25 16:02:19 -0400

    added editor for CombinedGridRoomStep

---
+++ RogueElements/MapGen/Grid/ICombinedGridRoomStep.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="ICombinedGridRoomStep.cs" company="Audino">
+// Copyright (c) Audino
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+
+namespace RogueElements
+{
+
+    public interface ICombinedGridRoomStep: IFloorPlanGenContext
+    {
+        RandRange MergeRate { get; set; }
+
+        SpawnList<GridCombo> Combos { get; set; }
+
+        List<BaseRoomFilter> Filters { get; set; }
+
+        ComponentCollection RoomComponents { get; set; }
+    }
+}
