13f5470aec428b638c15cebad5f89a5dc05e2816
AlexanderGroeger <alex.h.groeger@gmail.com>
2023-07-25 14:19:21 -0400

    added editor for BlobWaterStep

---
+++ RogueElements/MapGen/Tiles/Water/IBlobWaterStep.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IPerlinWaterStep.cs" company="Audino">
+// Copyright (c) Audino
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+
+namespace RogueElements
+{
+    public interface IBlobWaterStep : IWaterStep
+    {
+        RandRange Blobs { get; set; }
+
+        IntRange AreaScale { get; set; }
+
+        IntRange GenerateScale { get; set; }
+
+        // IBlobStencil BlobStencil { get; set; }
+    }
+}
