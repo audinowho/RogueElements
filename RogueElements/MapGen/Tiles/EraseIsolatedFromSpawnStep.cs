092715ec3e59982281d15b7d1abdd075ab6e9328
Audino <2676737+audinowho@users.noreply.github.com>
2023-12-21 20:39:53 -0800

    documentation fix

--- RogueElements/MapGen/Tiles/EraseIsolatedFromSpawnStep.cs
+++ RogueElements/MapGen/Tiles/EraseIsolatedFromSpawnStep.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="EraseIsolatedStep.cs" company="Audino">
+﻿// <copyright file="EraseIsolatedFromSpawnStep.cs" company="Audino">
 // Copyright (c) Audino
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
