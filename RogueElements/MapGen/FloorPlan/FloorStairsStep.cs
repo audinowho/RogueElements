b9b43a5e93fe1c5ae9fa8ccd6a2263b1bc1f44af
Audino <2676737+audinowho@users.noreply.github.com>
2025-01-20 13:38:00 -0800

    FloorStairsStep MinDistance

--- RogueElements/MapGen/FloorPlan/FloorStairsStep.cs
+++ RogueElements/MapGen/FloorPlan/FloorStairsStep.cs
@@ -25,15 +25,22 @@ namespace RogueElements
         {
         }
 
-        public FloorStairsStep(TEntrance entrance, TExit exit)
+        public FloorStairsStep(int minDistance, TEntrance entrance, TExit exit)
             : base(entrance, exit)
         {
+            this.MinDistance = minDistance;
         }
 
-        public FloorStairsStep(List<TEntrance> entrances, List<TExit> exits)
+        public FloorStairsStep(int minDistance, List<TEntrance> entrances, List<TExit> exits)
             : base(entrances, exits)
         {
+            this.MinDistance = minDistance;
         }
+
+        /// <summary>
+        /// The minimum distance in room adjacencies that starts and ends should be placed from each other.
+        /// </summary>
+        public int MinDistance { get; set; }
 
         /// <summary>
         /// Attempt to choose an outlet in a room with no entrance/exit, and updates their availability.  If none exists, default to null.
@@ -66,6 +73,28 @@ namespace RogueElements
                 {
                     free_indices.RemoveAt(roomIndex);
                     used_indices.Add(startRoom);
+
+                    // also transfer all adjacent rooms up to a depth
+                    Dictionary<RoomHallIndex, int> roomDistance = new Dictionary<RoomHallIndex, int>();
+
+                    void NodeAct(RoomHallIndex nodeIndex, int distance)
+                    {
+                        roomDistance[nodeIndex] = distance;
+
+                        // prefer not to remove by value, but we have no choice
+                        free_indices.Remove(nodeIndex.Index);
+                        used_indices.Add(nodeIndex.Index);
+                    }
+
+                    List<RoomHallIndex> GetAdjacentsLimited(RoomHallIndex nodeIndex)
+                    {
+                        if (roomDistance[nodeIndex] + 1 < this.MinDistance)
+                            return map.RoomPlan.GetAdjacents(nodeIndex);
+                        else
+                            return new List<RoomHallIndex>();
+                    }
+
+                    Graph.TraverseBreadthFirst(new RoomHallIndex(startRoom, false), NodeAct, GetAdjacentsLimited);
                 }
 
                 return start;
